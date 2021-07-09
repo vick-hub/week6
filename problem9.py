7f1a0d93f8e7f35e63442c8e7c80db1ef1b3e770
vick-hub <amondev00@gmail.com>
2021-07-09 23:47:10 +0300

    Modified

--- problem9.py
+++ problem9.py
@@ -4,30 +4,33 @@ import math
 
 
 def equations_of_motion(vo, a, t):
-    v = float(vo + a * t)
+    v = float(vo + (a * t))
     print(v)
 
 
 def equations_of_motion1(vo, a, ro, r):
-    v = math.sqrt((vo ** 2) + (2 * a * (r - ro)))
-    print(v)
+    v1 = math.sqrt((vo ** 2) + (2 * a * (r - ro)))
+    print(v1)
+
+
+functions = {1: equations_of_motion, 2: equations_of_motion1}
+print(f"which function will you use to solve for v: ")
 
 
 def main():
-    functions = {'1': equations_of_motion, '2': equations_of_motion1}
-    print(f"which function will you use to solve for v: ")
     for k, v in functions.items():
-        print(k, '-->', v)
-    selected = input('Enter the function: ')
-    vo = float(input("vo: "))
-    a = float(input("a: "))
-    t = float(input("t: "))
-    if selected == 1:
-        print(equations_of_motion(vo, a, t))
-    else:
-        ro = float(input("ro: "))
-        r = float(input("r: "))
-        print(equations_of_motion1(vo, a, ro, r))
+        print(v, '=', k)
+    selected = input('Enter the function 1 or 2: ')
+    for i in selected:
+        vo = float(input("vo: "))
+        a = float(input("a: "))
+        t = float(input("t: "))
+        if i == 1:
+            print(f"v: {equations_of_motion(vo, a, t)}")
+        else:
+            ro = float(input("ro: "))
+            r = float(input("r: "))
+            print(equations_of_motion1(vo, a, ro, r))
     return os.EX_OK
 
 
